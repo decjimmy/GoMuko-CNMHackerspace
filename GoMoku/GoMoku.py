f3561ecab5f534733830e038a7f306ad11a2ca1b
Jimmy Alexander <decjimmy@aol.com>
2020-06-12 15:41:46 -0600

    added green floor

--- GoMoku/GoMoku.py
+++ GoMoku/GoMoku.py
@@ -4,6 +4,7 @@ arcade.set_background_color(arcade.csscolor.SKY_BLUE)
 
 arcade.start_render()
 #drawing code goes here
+arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.csscolor.GREEN)
 arcade.finish_render()
 
 arcade.run()
