48b107dced76766732e6d6462e438cd8f4041f42
Jimmy Alexander <decjimmy@aol.com>
2020-06-12 15:33:37 -0600

    added coloured background

--- GoMoku/GoMoku.py
+++ GoMoku/GoMoku.py
@@ -1,4 +1,9 @@
 import arcade
 arcade.open_window(600,600, "Drawining example")
+arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+
+arcade.start_render()
+#drawing code goes here
+arcade.finish_render()
 
 arcade.run()
