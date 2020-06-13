af4b2b8e1bed120d925a78223fa8ace950935929
Jimmy Alexander <decjimmy@aol.com>
2020-06-13 12:26:20 -0600

    added print board function for console game

--- GoMoku/Game.py
+++ GoMoku/Game.py
@@ -1,20 +1,33 @@
 class Game(object):
-    """The game object of Gomoku."""
+    """The game object of Gomoku.
+       0 represents empty spot
+       1 represents white piece
+       2 represents black piece"""
     def __init__(self):
         self.board = []
         self.generate_board()
     def generate_board(self):
         """Creates a 15 by 15 grid in the form of an array where the game will be played"""
-        """zeros represent no placed gamepiece"""
         self.board = []
         for x in range(0,15):
             self.board.append([])
             for y in range(0,15):
                 self.board[x].append(0)
-
-g = Game()
-g.generate_board()
-print (g.board)
+    def place_piece(self,player, x, y):
+        """player: the player playing 1 is white, 2 is black
+           x: x coord of place to place piece
+           y: y coord of place to place piece """
+        
+        if player == 1 or player == 2:
+            self.board[x][y] = player
+        else:
+            """TODO: add error protection"""
+            print('invalid player')
+if __name__ == "__main__":
+    g = Game()
+    g.generate_board()
+    g.place_piece(1,2,3)
+    print (g.board)
 
 
 
--- GoMoku/GoMokuConsole.py
+++ GoMoku/GoMokuConsole.py
@@ -1,14 +1,24 @@
 import Game
 class GoMokuConsole(object):
     """description of class"""
-    def __init__():
-        game = Game.Game()
-    def drawBoard():
+    def __init__(self):
+        self.game = Game.Game()
+    def drawBoard(self):
+        board = self.game.board
+        for x in range(len(board)):
+            for y in range(len(board[x])):
+                print (board[x][y], end=' ')
+            print()
+
+    def placePiece(self, player, x,y):
+        self.game.place_piece(player, x, y)
+        
+    def calc(self):
         pass
-    def placePiece(x,y):
-        pass
-    def calc():
-        pass
+
+g = GoMokuConsole()
+g.placePiece(1, 3,4)
+g.drawBoard()
 
 
 
