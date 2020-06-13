0a26235385d62817990f88f309420d793684097e
Jimmy Alexander <decjimmy@aol.com>
2020-06-13 11:57:23 -0600

    added generation of a basic empty board

--- GoMoku/Game.py
+++ GoMoku/Game.py
@@ -1,7 +1,20 @@
 class Game(object):
     """The game object of Gomoku."""
-    def __init__():
-        pass
+    def __init__(self):
+        self.board = []
+        self.generate_board()
+    def generate_board(self):
+        """Creates a 15 by 15 grid in the form of an array where the game will be played"""
+        """zeros represent no placed gamepiece"""
+        self.board = []
+        for x in range(0,15):
+            self.board.append([])
+            for y in range(0,15):
+                self.board[x].append(0)
+
+g = Game()
+g.generate_board()
+print (g.board)
 
 
 
--- GoMoku/GoMokuConsole.py
+++ GoMoku/GoMokuConsole.py
@@ -1,4 +1,15 @@
+import Game
 class GoMokuConsole(object):
     """description of class"""
+    def __init__():
+        game = Game.Game()
+    def drawBoard():
+        pass
+    def placePiece(x,y):
+        pass
+    def calc():
+        pass
+
+
 
 
