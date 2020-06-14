e85028ce72e45b2cdda2e408c9f11d1be8110a82
darian23 <64653612+darian23@users.noreply.github.com>
2020-06-14 02:05:30 -0600

    Added diagonal detection

--- GoMoku/Game.py
+++ GoMoku/Game.py
@@ -13,8 +13,8 @@ class Game(object):
         self.WIN_LENGTH = 3
         self.player = self.BLACK  # black makes the first move
         self.move = 0
-        self.BBoard = np.zeros((self.WIDTH,self.HEIGHT))
-        self.WBoard = np.zeros((self.WIDTH,self.HEIGHT))
+        self.BBoard = np.zeros((self.WIDTH, self.HEIGHT))
+        self.WBoard = np.zeros((self.WIDTH, self.HEIGHT))
  
  
     def place_piece(self, x, y):
@@ -26,7 +26,7 @@ class Game(object):
             assert self.BBoard[x][y] == 0 and self.WBoard[x][y] == 0, \
                 "Coordinate ({0}, {1}) already occupied".format(x, y)
         except AssertionError as msg:
-            print("place_piece:",msg)
+            print("place_piece:", msg)
         else:
             self.move += 1
             if self.player == self.BLACK:
@@ -42,10 +42,23 @@ class Game(object):
            Return NO_WINNER if there are still empty spaces"""
         result = self.NO_WINNER
         for (board, player) in [(self.BBoard, self.BLACK), (self.WBoard, self.WHITE)]:
-            for board in [board, np.transpose(board)]:
+            for board in [board, np.rot90(board)]:
                 for row in board:
                     if "1. "*self.WIN_LENGTH in np.array2string(row):
                        result = player
+ 
+                # Check diagonals by reshaping 2D array into 1D and taking slices
+                # that correspond to all possible diagonals of length WIN_LENGTH
+                (h, w) = board.shape # Can't use WIDTH and HEIGHT because board may be transposed
+                board = board.reshape((w*h,)) # Make array 1D
+                step = w + 1 # next element in diagonal is one more ahead than the width
+                for j in range(h-self.WIN_LENGTH+1):
+                    for i in range(w-self.WIN_LENGTH+1):
+                        start = j*w+i
+                        dSlice = board[start:start+step*self.WIN_LENGTH:step]
+                        if np.array_equal(dSlice, np.ones((self.WIN_LENGTH,))):
+                            result = player
+            
         if result == self.NO_WINNER:
             if np.array_equal(self.WBoard+self.BBoard, np.ones((self.WIDTH, self.HEIGHT))):
                 result = self.DRAW
@@ -77,21 +90,17 @@ if __name__ == "__main__":
     g.place_piece(0, g.HEIGHT)
     g.place_piece(g.WIDTH, 0)
 
-    # Test that black wins after placing 3 in a row
-    g.place_piece(0, 0)
-    g.check_win()
-    g.place_piece(5, 5)
-    g.check_win()
-    g.place_piece(1, 0)
-    g.check_win()
-    g.place_piece(1, 1)
-    g.check_win()
-    g.place_piece(2, 0)
-    g.check_win()
+    # Test that black wins after placing 3 in a diagonal
+    for x in range(g.WIN_LENGTH):
+        g.place_piece(x, (x+1)*2-1)
+        g.check_win()
+        g.place_piece(x, x)
+        g.check_win()
+
   
 
     # TODO DR Make more comprehensive tests:
-    #   Test row and column wins
+    #   Test row, column and upward diagonal wins
     #   Force a draw by filling board with alternating pieces
 
 
